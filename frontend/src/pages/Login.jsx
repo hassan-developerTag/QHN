6e75e0d4d95d7ce597081af1a517e717626e6b67
Hassan Mehmood <hassanmehmood@developertag.com>
2025-02-11 23:55:10 +0500

    .

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -31,7 +31,7 @@ const Login = () => {
   const onSubmit = async (data) => {
     try {
       setIsLoading(true);
-      const response = await axios.post("https://qhn-s4pw7mdah-hassan-mehmoods-projects-2e3cea23.vercel.app/auth/login", data);
+      const response = await axios.post("https://qhn-hassan-mehmoods-projects-2e3cea23.vercel.app/", data);
 
       if (response.data.success) {
         // Store user data
